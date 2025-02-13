3c313e13536e83a0ff362679fc21b0043001e7aa
hachi8833 <hachi8833@gmail.com>
2025-02-13 12:15:04 +0900

    Replace MainActivity.kt

--- app/src/main/java/com/example/hotwireexample/MainActivity.kt
+++ app/src/main/java/com/example/hotwireexample/MainActivity.kt
@@ -5,16 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dev.hotwire.navigation.activities.HotwireActivity
+import dev.hotwire.navigation.navigator.NavigatorConfiguration
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+class MainActivity : HotwireActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+
+  override fun navigatorConfigurations() = listOf(
+    NavigatorConfiguration(
+      name = "main",
+      startLocation = "http://10.0.2.2:3000",
+      navigatorHostId = R.id.main_nav_host
+    )
+  )
 }
