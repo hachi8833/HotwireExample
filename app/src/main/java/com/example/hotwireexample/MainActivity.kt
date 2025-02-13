40dc2656f27ead1004337162a2030ec06022d279
hachi8833 <hachi8833@gmail.com>
2025-02-13 13:10:14 +0900

    Add R to import

--- app/src/main/java/com/example/hotwireexample/MainActivity.kt
+++ app/src/main/java/com/example/hotwireexample/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.hotwireexample
 import android.os.Bundle
 import dev.hotwire.navigation.activities.HotwireActivity
 import dev.hotwire.navigation.navigator.NavigatorConfiguration
+import com.example.hotwireexample.R
 
 class MainActivity : HotwireActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
