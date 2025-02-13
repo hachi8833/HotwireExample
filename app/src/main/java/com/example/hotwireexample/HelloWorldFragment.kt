37b912c8f51293396b10adb488ddf36702e4ef1b
hachi8833 <hachi8833@gmail.com>
2025-02-13 16:47:19 +0900

    Fix imports

--- app/src/main/java/com/example/hotwireexample/HelloWorldFragment.kt
+++ app/src/main/java/com/example/hotwireexample/HelloWorldFragment.kt
@@ -1,5 +1,13 @@
 package com.example.hotwireexample
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import dev.hotwire.navigation.destinations.HotwireDestinationDeepLink
+import dev.hotwire.navigation.fragments.HotwireFragment
+import com.example.hotwireexample.main.*
+
 @HotwireDestinationDeepLink(uri = "hotwire://fragment/hello_world")
 class HelloWorldFragment: HotwireFragment() {
   override fun onCreateView(
